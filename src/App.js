8530b8be442c59e073acf81ad63bc44d19466347
rokib97 <hasanrokib1997@gmail.com>
2022-10-15 13:42:56 +0600

    google signinup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
+import "./App.css";
+import app from "./Firebase/firebase.init";
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const provider = new GoogleAuthProvider();
+  const handkeGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => console.log(error));
+  };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.email ? (
+        <button onClick={handleSignOut}>Google Sign Out</button>
+      ) : (
+        <button onClick={handkeGoogle}>Google Sign in</button>
+      )}
+      {user.email && (
+        <div>
+          <h1>User name: {user.displayName}</h1>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIQyjLTtqNnpZh26aoMOhRkvQ8lVHCf4A",
+  authDomain: "practice-firebase-77d6a.firebaseapp.com",
+  projectId: "practice-firebase-77d6a",
+  storageBucket: "practice-firebase-77d6a.appspot.com",
+  messagingSenderId: "845971068951",
+  appId: "1:845971068951:web:b9449596bb248f7edae452",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
